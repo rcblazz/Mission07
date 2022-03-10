1a103e1a8a74721a61257557f0830558ea3b90c1
rcblazz <beccablazzard@gmail.com>
2022-03-10 12:48:24 -0700

    Mission09pt2

---
+++ Mission07/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission07.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission07.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        
+        public CartSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
